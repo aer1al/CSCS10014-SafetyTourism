9c8b448a9faa23bf5968e6bc426753e66913bcd3
Viet Khoa <130777227+aer1al@users.noreply.github.com>
2025-12-21 19:24:18 +0700

    fix comment

--- SafetyRoute/backend/rag_engine/config.py
+++ SafetyRoute/backend/rag_engine/config.py
@@ -2,5 +2,6 @@
 
 # Cấu hình kết nối Neo4j
 NEO4J_URI = "neo4j://localhost:7687"
-NEO4J_AUTH = ("neo4j", "12345678") #
+NEO4J_AUTH = ("neo4j", "12345678") # mật khẩu đặt tùy ý
+
 OLLAMA_MODEL = "gemma:2b"
